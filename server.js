dd0e2d7ad33e7e09dd39be00e651b8bc05e6b532
Killian <killianr479@gmail.com>
2021-10-12 14:12:05 +0200

    Ajout affichage pseudo

--- server.js
+++ server.js
@@ -54,6 +54,7 @@ app.use('/new', (req, res) => {
             .then(partie => {
                 console.log("partie_id", partie.id);
                 res.cookie("id_partie", partie.id);
+                res.cookie("pseudo", partie.pseudo)
                 res.end("prout")
                 //console.log(res.cookie);
             })
